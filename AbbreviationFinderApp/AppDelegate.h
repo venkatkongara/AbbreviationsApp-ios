a6e920da1cb08e3debfea3af1a3dd06ef84534ee
Venkat kongara <venkat.ios.13@gmail.com>
2015-10-02 09:32:05 -0700

    Initial Commit

---
+++ AbbreviationFinderApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AbbreviationFinderApp
+//
+//  Created by siva kongara on 10/2/15.
+//  Copyright © 2015 siva kongara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AbbreviationFinderApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AbbreviationFinderApp
+//
+//  Created by siva kongara on 10/2/15.
+//  Copyright © 2015 siva kongara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
